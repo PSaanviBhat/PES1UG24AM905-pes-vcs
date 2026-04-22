d5a667dae4bb1da4818d7cde0f2ef4dc75607f96
P Saanvi <psaanvibhat@outlook.com>
2026-04-22 19:16:32 +0530

    Phase 4: update HEAD after commit

--- commit.c
+++ commit.c
@@ -217,7 +217,16 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         return -1;
     }
 
-   
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    if (head_update(commit_id_out) != 0) {
+        return -1;
+    }
 
     return 0;
 }
