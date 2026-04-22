2f4acd4c72485dcb1bcce67518078d539af5eaa1
P Saanvi <psaanvibhat@outlook.com>
2026-04-22 18:17:28 +0530

    Phase 2: return root tree hash

--- tree.c
+++ tree.c
@@ -196,3 +196,9 @@ static int write_tree_level(const Index *index, const char *prefix, ObjectID *id
     free(data);
     return rc;
 }
+int tree_from_index(ObjectID *id_out) {
+    Index index;
+    if (index_load(&index) != 0) return -1;
+
+    return write_tree_level(&index, "", id_out);
+}
