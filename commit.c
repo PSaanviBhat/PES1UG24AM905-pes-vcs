73c0de389d1aeeb951d9347bd40f76ce275a76c9
P Saanvi <psaanvibhat@outlook.com>
2026-04-22 19:16:16 +0530

    Phase 4: serialize and write commit object

--- commit.c
+++ commit.c
@@ -211,6 +211,12 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     commit.timestamp = (uint64_t)time(NULL);
     snprintf(commit.message, sizeof(commit.message), "%s", message);
 
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) != 0) {
+        return -1;
+    }
+
    
 
     return 0;
